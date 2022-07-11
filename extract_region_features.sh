83fe20b3e4416a763fff6acffb4f440061bd8a51
Ubuntu <jwyang@dev4jwatdlg2.b2yryog3dfmuxkep313goz21qf.xx.internal.cloudapp.net>
2022-07-11 16:09:33 +0000

    fix command

--- extract_region_features.sh
+++ extract_region_features.sh
@@ -26,7 +26,7 @@ TEST.DETECTIONS_PER_IMAGE 100 \
 # MODEL.CLIP.OFFLINE_RPN_POST_NMS_TOPK_TEST 100 \
 
 # # RN50x4, LVIS 1203 concepts
-# python3 ./tools/train_net.py \
+# python3 ./tools/extract_region_features.py \
 # --config-file ./configs/LVISv1-InstanceSegmentation/CLIP_fast_rcnn_R_50_C4_zsinf.yaml \
 # MODEL.WEIGHTS ./pretrained_ckpt/regionclip/regionclip_pretrained-cc_rn50x4.pth \
 # MODEL.CLIP.TEXT_EMB_PATH ./pretrained_ckpt/concept_emb/lvis_1203_cls_emb_rn50x4.pth \
